a6b899ba1704c8bb8679aeaf4177d697c9d383cc
John Shearer <john@ultranaut.com>
2021-03-15 11:56:26 -0400

    day 6 - made the whole thing asynchronous

--- 06 - Type Ahead/type.js
+++ 06 - Type Ahead/type.js
@@ -1,3 +1,14 @@
+const debounce = (fn, delay) => {
+  let timer = null;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+};
+
+const formatNumber = (num) =>
+  num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
 const endpoint =
   'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
@@ -7,16 +18,24 @@ fetch(endpoint)
   .then((resp) => resp.json())
   .then((data) => cities.push(...data));
 
-const findMatches = (input, cities) => {
+const findMatches = async (input) => {
+  const cities = [];
+  await fetch(endpoint)
+    .then((res) => res.json())
+    .then((data) => {
+      cities.push(...data);
+    });
+
   const pattern = new RegExp(input, 'gi');
   return cities.filter(
     (data) => data.city.match(pattern) || data.state.match(pattern)
   );
 };
 
-const displayMatches = (e) => {
+const displayMatches = async (e) => {
   const input = e.target.value;
-  const matches = findMatches(input, cities);
+  console.log('input: %s', input);
+  const matches = await findMatches(input, cities);
   const pattern = new RegExp(input, 'gi');
   suggestions.innerHTML = matches
     .map((data) => {
@@ -28,7 +47,7 @@ const displayMatches = (e) => {
         pattern,
         `<span class="hl">${input}</span>`
       );
-      const population = data.population;
+      const population = formatNumber(data.population);
       return `
       <li>
         <span class="name">${city}, ${state}</span>
@@ -42,4 +61,4 @@ const displayMatches = (e) => {
 const searchInput = document.querySelector('.search-form .search');
 const suggestions = document.querySelector('.search-form .suggestions');
 
-searchInput.addEventListener('input', displayMatches);
+searchInput.addEventListener('input', debounce(displayMatches, 400));
